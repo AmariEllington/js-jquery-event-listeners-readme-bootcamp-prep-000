934a7f3966422c0e29e736cfe57f4ba000a3ca61
Amari Ellington <amari.l.ellington@gmail.com>
2019-02-08 23:41:58 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,35 +1,37 @@
+function getIt(){
+  $('p').on('click', function(){
+      alert("Hey!");//action you want taken
+  });
+}
+
+ function frameIt(){
+  $('img').on('load', function(){
+    $('img').addClass('tasty')
+  });
+}
+
+ function pressIt(){
+$(document).on('keydown', function(key) {
+if(key.which == 71){
+alert('G was pressed');
+}
+});
+}
+
+ function submitIt(){
+$('form').on('submit', function() {
+    alert('Your form is going to be submitted now.');
+    });
+}
+
 //define functions here
-function getIt() {
-  $('p').on('click', function){
-    window.alert("Hey!");
-  });
-}
-
-function frameIt(){
-  $("img").on("load", function(){
-    $("img").addClass("tasty");
-  });
-}
-
-function pressIt(){
-  $("input").on("keydown", function(e){
-    if (e.which === 71){
-      window.alert("G was pressed.");
-    }
-  });
-}
-
-function submitIt(){
-  $("form").on("submit", function(){
-    window.alert("Your form is going to be submitted now.");
-  });
-}
 
 $(document).ready(function(){
 
+
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 // call functions here
-getIt();
-frameIt();
-pressIt();
-submitIt();
 });
